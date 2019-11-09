f802d617c90db5c168d6109e2b949cc2d41d6314
mohsr <mkrizvi98@gmail.com>
2019-11-09 16:36:02 -0500

    mongodb integration

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ var crypto     = require('crypto');
 var bodyparser = require('body-parser');
 var app = express();
 
+// Configure MongoDB
+var mongoUri = 'mongodb://' + process.env.DBUSER + ':' + process.env.DBPASS + 
+               '@' + process.env.DBURL
+var mongo  = require('mongodb').MongoClient;
+var format = require('util').format;
+var db = mongo.connect(mongoUri, function(error, dbconnection) {
+    db = dbconnection;
+});
+
 /* Set up app for use. */
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
